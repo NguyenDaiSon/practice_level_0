27721f927b4c43971624dce9f4c9c94426e1a37b
Nguyen Dai Son <daisonit@gmail.com>
2024-05-12 23:59:11 +0800

    06ef84e8

--- golang/foundations/slices/06ef84e8.go
+++ golang/foundations/slices/06ef84e8.go
@@ -7,6 +7,21 @@ import (
 func main() {
 	s := []int{0, 1, 2, 3, 4, 5, 6}
 	fmt.Println(s)
+	fmt.Println(len(s))
+	fmt.Println(cap(s))
 
-	fmt.Print(s[:4]) // 0 1 2 3
+	s1 := s[:4]
+	fmt.Println(s1)
+	fmt.Println(len(s1))
+	fmt.Println(cap(s1))
+
+	s2 := s[3:]
+	fmt.Println(s2)
+	fmt.Println(len(s2))
+	fmt.Println(cap(s2))
+
+	s[3] = 9
+	fmt.Println(s)
+	fmt.Println(s1)
+	fmt.Println(s2)
 }
